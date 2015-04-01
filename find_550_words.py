827c723d0d38787b33f58c63c69c5d782214fddc
bugriyiryna <iryna.buhrii@gmai.com>
2015-04-01 22:43:39 +0300

    Revert "Revert "Function and stopwords list added""
    This reverts commit 84f3c8338246449e435d8792a1beeb8cac5f6478.

---
+++ find_550_words.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+
+"""
+# Please, be careful! Python3 and NLTK 3.0.
+
+import nltk
+from nltk.corpus import PlaintextCorpusReader
+stopwords=nltk.corpus.stopwords.words('ukrainian1')
+def corpus_checking(corpus_root):
+    """Function for personal corpus (all txt files in "corpus_root" ) creation 
+    and checking the number of words 
+    """
+    reader = PlaintextCorpusReader(corpus_root, '.*\.txt')
+    words=set(w.lower() for w in reader.words() if w.isalpha() and w.lower() not in stopwords )
+    if len(words) < 100:
+        print ("Додай, будь-ласка, мила, ще трохи відгуків.")
+        print (len(words))
+    else:
+        print (words)
+        f=open(corpus_root+'\wordslist.txt','w')
+        for word in words:
+            f.write(word+'\n')
+        f.close()
+    
+corpus_root = 'E:\Files'
+corpus_checking(corpus_root)
