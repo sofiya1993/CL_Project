2bee60c43b171a7688b0d464cb52285292468683
Oksanka1881 <oksanka_kitty@online.ua>
2015-04-09 09:26:18 +0300

    Курсова
    1 частина

---
+++ Kulba/find_550_words_27.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+
+"""
+# Please, be careful! Python2.7 and NLTK 2.0.
+
+import nltk
+from nltk.corpus import PlaintextCorpusReader
+# set path to file with ukrainian stopwords
+stopwords_path = 'C:\Kulba'
+stopwords=PlaintextCorpusReader(stopwords_path, 'ukrainian1', encoding='utf-8').words()
+def corpus_checking(corpus_root):
+    """Function for personal corpus (all txt files in "corpus_root" ) creation 
+    and checking the number of words 
+    """
+    reader = PlaintextCorpusReader(corpus_root, '.*\.txt', encoding='utf-8')
+    words=set(w.lower() for w in reader.words() if w.isalpha() and w.lower() not in stopwords )
+    if len(words) < 100:
+        print ("Додай, будь-ласка, мила, ще трохи відгуків.")
+        print (len(words))
+    else:
+        for w in words:
+            print w
+        f=open(corpus_root+'\Kulba_wordslist.txt','w')
+        for word in words:
+            f.write(word.encode('utf-8')+'\n')
+        f.close()
+    
+corpus_root = 'C:\Kulba'
+corpus_checking(corpus_root)
