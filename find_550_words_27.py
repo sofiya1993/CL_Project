8186b036e69b9a717be761873f7463d9121092be
Irynahanchevska <irahanchevska1404@gmail.com>
2015-04-17 21:48:28 +0300

    курсова
    Завершена перша частина

--- find_550_words_27.py
+++ find_550_words_27.py
@@ -4,26 +4,29 @@ Spyder Editor
 
 
 """
-# Please, be careful! Python3 and NLTK 3.0.
+# Please, be careful! Python2.7 and NLTK 2.0.
 
 import nltk
 from nltk.corpus import PlaintextCorpusReader
-stopwords=nltk.corpus.stopwords.words('ukrainian1')
+# set path to file with ukrainian stopwords
+stopwords_path = 'C:\Hanchevska'
+stopwords=PlaintextCorpusReader(stopwords_path, 'ukrainian1', encoding='utf-8').words()
 def corpus_checking(corpus_root):
     """Function for personal corpus (all txt files in "corpus_root" ) creation 
     and checking the number of words 
     """
-    reader = PlaintextCorpusReader(corpus_root, '.*\.txt')
+    reader = PlaintextCorpusReader(corpus_root, '.*\.txt', encoding='utf-8')
     words=set(w.lower() for w in reader.words() if w.isalpha() and w.lower() not in stopwords )
     if len(words) < 100:
         print ("Додай, будь-ласка, мила, ще трохи відгуків.")
         print (len(words))
     else:
-        print (words)
+        for w in words:
+            print w
         f=open(corpus_root+'\Hanchevska_wordslist.txt','w')
         for word in words:
-            f.write(word+'\n')
+            f.write(word.encode('utf-8')+'\n')
         f.close()
     
-corpus_root = 'C:\Users\User\Documents\GitHub\CL_Project'
+corpus_root = 'C:\Hanchevska'
 corpus_checking(corpus_root)
